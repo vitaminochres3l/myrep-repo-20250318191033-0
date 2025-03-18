26a92e6f54ce0c44159933c465c4463248000903
vitaminochres3l <vitamin-ochres-3l@icloud.com>
2025-03-18 23:57:07 +0400

    Update script_0.py

--- script_0.py
+++ script_0.py
@@ -1,5 +1,4 @@
-14else:
-    print(" Connection failed")
+15    print(" Connection failed")
 
 print(f"Performing blockchain info check: w3.eth.get_accounts()")
 print(f"Web3 is connected: {w3.is_connected}")
